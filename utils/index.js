26c69a1eeb0be2e191a66cf445e66d243d7abba9
Casey Nguyen <116910297+caseyhnguyen@users.noreply.github.com>
2023-11-26 22:28:36 -0800

    Delete utils directory

--- utils/index.js
+++
@@ -1,5 +0,0 @@
-import millisToMinutesAndSeconds from "./millisToMinutesAndSeconds";
-import useSpotifyAuth from "./useSpotifyAuth";
-import useSpotifyTracks from "./useSpotifyTracks";
-
-export { millisToMinutesAndSeconds, useSpotifyAuth, useSpotifyTracks };

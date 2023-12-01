24917f19823edb1c3772c64d9a9e46b8733bd39e
Caroline Tran <89322465+carolinentran@users.noreply.github.com>
2023-12-01 06:22:43 -0800

    Layout and hard coded content of post summary

--- app/PostSummaryScreen.js
+++ app/PostSummaryScreen.js
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   Dimensions,
   Pressable,
+  TextInput,
 } from "react-native";
 import images from "../assets/Images/images";
 import { colors } from "../assets/Themes/colors";
@@ -20,98 +21,118 @@ const totalGapSize = (itemPerRow - 1) * gap;
 const rowWidth = windowWidth * 0.8 + totalGapSize;
 
 const PostSummaryScreen = ({ route, navigation }) => {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState("");
+
   const songData = route.params?.songData || {};
   const artistNames =
     songData && songData.artists
       ? songData.artists.join(", ")
       : "Unknown Artist";
+
   return (
     <SafeAreaView style={defaultStyles.container}>
-      <View style={styles.postSumOuterContainer}>
+      <Text style={styles.subHeader}>Your Post Draft</Text>
+      <View style={styles.outerContainer}>
         {songData && songData.title && (
-          <View style={styles.PostSumSongContainer}>
+          <View style={styles.songContainer}>
             <Image
               source={{ uri: songData.imageUrl }}
-              style={styles.postSumAlbumCover}
+              style={styles.albumCover}
             />
-            <View style={styles.titleAndArtist}>
-              <Text style={styles.postSumTitle} numberOfLines={1}>
-                {songData.title}
+            <View style={styles.smallSelectionCol}>
+              <Image
+                source={images.matchaLatte.pic}
+                style={styles.smallImage}
+              />
+              <Text style={styles.smallText}>{images.matchaLatte.label}</Text>
+              <Image
+                source={images.superHappyEmoji.pic}
+                style={styles.smallImage}
+              />
+              <Text style={styles.smallText}>
+                {images.superHappyEmoji.label}
               </Text>
-              <Text style={styles.postSumArtist} numberOfLines={1}>
-                {Array.isArray(songData.artists)
-                  ? songData.artists.join(", ")
-                  : songData.artists}
-              </Text>
-              <View style={styles.smallSelectionRow}>
-                <Image
-                  source={images.matchaLatte.pic}
-                  style={styles.smallImage}
-                />
-                <Image
-                  source={images.superHappyEmoji.pic}
-                  style={styles.smallImage}
-                />
-                <Image source={images.working.pic} style={styles.smallImage} />
-              </View>
+
+              <Image source={images.working.pic} style={styles.smallImage} />
+              <Text style={styles.smallText}>{images.working.label}</Text>
             </View>
             {/* other song details */}
           </View>
         )}
-        <Text>insert drowndown here</Text>
-
-        <Pressable
-          style={defaultStyles.button}
-          onPress={() =>
-            navigation.navigate("FeedScreen", {
-              screen: "Feed",
-              params: { songData },
-            })
-          }
-        >
-          <Text style={defaultStyles.buttonText}>
-            Jump to Feed (Testing Purposes)
+        <View style={styles.titleAndArtist}>
+          <Text style={styles.title} numberOfLines={2}>
+            {songData.title}
           </Text>
-        </Pressable>
+          <Text style={styles.artist} numberOfLines={1}>
+            {Array.isArray(songData.artists)
+              ? songData.artists.join(", ")
+              : songData.artists}
+          </Text>
+        </View>
+        <TextInput
+          style={styles.input}
+          multiline={true}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="Write a caption..."
+          keyboardType="numeric"
+        ></TextInput>
+
+        <View style={styles.buttonRow}>
+          <Text>drop</Text>
+
+          <Pressable
+            style={defaultStyles.button}
+            onPress={() =>
+              navigation.navigate("FeedScreen", {
+                screen: "Feed",
+                params: { songData },
+              })
+            }
+          >
+            <Text style={defaultStyles.buttonText}>Post</Text>
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
-  postSumOuterContainer: {
+  outerContainer: {
     width: windowWidth,
-    alignItems: "center",
+    // alignItems: "center",
     justifyContent: "flex-start",
     flexDirection: "column",
     backgroundColor: colors.offWhite75,
     borderRadius: 10,
     width: rowWidth + 10,
-    height: windowWidth * 0.8,
+    height: windowWidth * 1.1,
   },
-  postSumSongContainer: {
-    width: windowWidth,
-    alignItems: "center",
-    justifyContent: "flex-start",
+  songContainer: {
+    alignItems: "flex-start",
+    justifyContent: "space-between",
     flexDirection: "row",
     borderRadius: 10,
-    width: rowWidth + 10,
-    height: windowWidth * 0.3,
+    width: "95%",
+    height: windowWidth * 0.5,
     gap: 5,
-    paddingLeft: 10,
+    paddingLeft: 20,
+    paddingTop: 20,
   },
-  postSumAlbumCover: {
-    width: windowWidth * 0.25, // Adjust size as needed
-    height: windowWidth * 0.25, // Adjust size as needed
-    borderRadius: 10,
+  albumCover: {
+    width: windowWidth * 0.5, // Adjust size as needed
+    height: windowWidth * 0.5, // Adjust size as needed
+    // borderRadius: 10,
   },
-  postSumTitle: {
-    fontSize: 16,
+  title: {
+    fontSize: 14,
     fontWeight: "bold",
     color: "black", // Adjust color as needed
     marginTop: 10,
   },
-  postSumArtist: {
-    fontSize: 14,
+  artist: {
+    fontSize: 12,
     color: "black", // Adjust color as needed
     marginTop: 5,
   },
@@ -119,28 +140,17 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "flex-start",
-    marginRight: 20,
-    width: "60%",
+    paddingLeft: 20,
+    paddingTop: 20,
+    width: "75%",
   },
-  postSumSelectionRow: {
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "row",
-    gap: totalGapSize * 2,
-  },
-  postSumSelectionGrid: {
+  selectionGrid: {
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "column",
     gap: totalGapSize,
   },
-  postSumOuterContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    gap: totalGapSize,
-  },
-  postSumPostInProgress: {},
+  postInProgress: {},
   boldText: {
     fontSize: 15,
     fontWeight: "bold",
@@ -148,19 +158,38 @@ const styles = StyleSheet.create({
   },
   smallImage: {
     marginTop: 5,
-    width: windowWidth * 0.1,
-    height: windowWidth * 0.1,
+    width: windowWidth * 0.115,
+    height: windowWidth * 0.115,
   },
-  smallSelectionRow: {
-    justifyContent: "center",
+  smallText: {
+    fontSize: 10,
+    color: colors.darkGray,
+    paddingBottom: "2%",
+  },
+  smallSelectionCol: {
+    alignItems: "center",
+    flexDirection: "col",
+  },
+  buttonRow: {
     alignItems: "center",
     flexDirection: "row",
-    gap: 5,
+    justifyContent: "space-between",
+    width: "96%",
+    paddingLeft: 20,
   },
   subHeader: {
-    fontSize: 14,
+    fontSize: 16,
     fontWeight: "bold",
     color: colors.offWhite,
+    paddingBottom: 5,
+  },
+  input: {
+    height: 40,
+    marginTop: 12,
+    marginBottom: 50,
+    borderTopColor: colors.darkGray,
+    paddingLeft: 20,
+    width: "70%",
   },
 });
 

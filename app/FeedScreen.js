9fe7704668468ce27eb0a1df7ac224615f076299
Casey Nguyen <casey@scope-zero.com>
2023-12-04 22:37:50 -0800

    Addressed feed screen error

--- app/FeedScreen.js
+++ app/FeedScreen.js
@@ -14,7 +14,6 @@ import images from "../assets/Images/images";
 import Post from "../components/Post";
 import { colors } from "../assets/Themes/colors";
 import { styles as defaultStyles } from "../assets/Themes/default_style";
-import formatPlayedAt from "../utils/formatPlayedAt.js";
 
 import { textStyles } from "../assets/Themes/Text";
 
@@ -28,15 +27,7 @@ const rowWidth = windowWidth * 0.8 + totalGapSize;
 const FeedScreen = ({ route, navigation }) => {
   const caption = route.params?.caption || "";
 
-  const {
-    songData,
-    selectedThemeIcon,
-    selectedThemeIconText,
-    selectedEmotionIcon,
-    selectedEmotionIconText,
-    selectedActivityIcon,
-    selectedActivityIconText,
-  } = route.params;
+  const songData = route.params?.songData || {};
   const artistNames =
     songData && songData.artists
       ? songData.artists.join(", ")
@@ -44,55 +35,22 @@ const FeedScreen = ({ route, navigation }) => {
 
   return (
     <SafeAreaView style={defaultStyles.container}>
-      <Text style={styles.subHeader}>Posts</Text>
-      <View style={styles.outerContainer}>
-        {songData && songData.title && (
-          <View style={styles.songContainer}>
-            <Image
-              source={{ uri: songData.imageUrl }}
-              style={styles.albumCover}
-            />
+      <Text style={textStyles.subHeader}>Posts</Text>
 
-            <View style={styles.smallSelectionCol}>
-              {selectedThemeIcon && (
-                <Image source={selectedThemeIcon} style={styles.smallImage} />
-              )}
-              <Text style={styles.smallText}>{selectedThemeIconText}</Text>
-              {selectedEmotionIcon && (
-                <Image source={selectedEmotionIcon} style={styles.smallImage} />
-              )}
-              <Text style={styles.smallText}>{selectedEmotionIconText}</Text>
-
-              {selectedActivityIcon && (
-                <Image
-                  source={selectedActivityIcon}
-                  style={styles.smallImage}
-                />
-              )}
-              <Text style={styles.smallText}>{selectedActivityIconText}</Text>
-            </View>
-            {/* other song details */}
-          </View>
-        )}
-        <View style={styles.titleAndArtist}>
-          <Text style={styles.title} numberOfLines={2}>
-            {songData.title}
-          </Text>
-          <Text style={styles.artist} numberOfLines={1}>
-            {Array.isArray(songData.artists)
-              ? songData.artists.join(", ")
-              : songData.artists}
-          </Text>
-          <Text style={styles.playedAt} numberOfLines={1}>
-            {formatPlayedAt(songData.played_at)}
-          </Text>
-        </View>
-        <Text style={styles.caption}>Wow this is so cool!</Text>
-      </View>
+      <Post
+        dimensions={{
+          windowWidth: windowWidth,
+          gap: gap,
+          totalGapSize: totalGapSize,
+          itemPerRow: itemPerRow,
+          rowWidth: rowWidth,
+        }}
+        songData={songData}
+        // navigation = {navigation}
+      ></Post>
     </SafeAreaView>
   );
 };
-const layout = StyleSheet.create({})
-
+const layout = StyleSheet.create({});
 
 export default FeedScreen;

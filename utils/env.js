5ce971dfc4f54482f7b933ec210ca20e439d8301
Sophie Wu <swu.lee02@gmail.com>
2023-12-06 14:23:47 -0800

    Add playlist details back in

--- utils/env.js
+++ utils/env.js
@@ -1,9 +1,9 @@
 import { Platform } from "react-native";
 
 // ***** TODO: Fill in your constants here ***** //
-const CLIENT_ID = "d80243faaa8e49278e824c2ce5971e8b";
-const CLIENT_SECRET = "dd8697a7f8314f15b542e4f14ad99424";
-const REDIRECT_URI = "exp://10.31.225.25:8082"; // TODO: Replace this with your own redirect URI
+const CLIENT_ID = "e14db2430bf94e3d8eb0faf382726f97";
+const CLIENT_SECRET = "ee54013184394b40a32db56d967bc0a8";
+const REDIRECT_URI = "exp://10.28.162.221:8081r"; // TODO: Replace this with your own redirect URI
 const ALBUM_ID = "2nLOHgzXzwFEpl62zAgCEC?si=Vy8vkAwuT-GJ_nEKsoo2DA"; // By default, this is the Weeknd's album "DAWN FM"
 // ********************************************* //
 
